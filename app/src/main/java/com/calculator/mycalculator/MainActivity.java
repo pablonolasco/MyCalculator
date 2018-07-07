afd3689e2a5523f64c5fd0987bbd962ae3a0e843
pablonolasco <pablo.nolasco@live.com.mx>
2018-07-06 21:38:58 -0500

    Se agregaron las Clase
    Se agregaron las clases para hacer las operaciones

--- app/src/main/java/com/calculator/mycalculator/MainActivity.java
+++ app/src/main/java/com/calculator/mycalculator/MainActivity.java
@@ -1,13 +1,25 @@
 package com.calculator.mycalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etInput)
+    EditText etInput;
+    @BindView(R.id.contentMain)
+    RelativeLayout contentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
---
+++ app/src/main/java/com/calculator/mycalculator/model/OnResolveCallback.java
@@ -0,0 +1,7 @@
+package com.calculator.mycalculator.model;
+
+public interface OnResolveCallback {
+    void onShowMessage(int errorRes);
+    void onIsEditing();
+
+}
